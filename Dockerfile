86e6d1582533baa6bbf75f92e8b0a4f5e081658a
Bhavya <bhavyajha1404@gmail.com>
2024-11-25 16:13:27 +0530

    debugging

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,17 @@
-# Use an official Python image as the base image
+# Start from the base Python image
 FROM python:3.8-slim
 
-# Set the working directory in the container
+# Set working directory
 WORKDIR /app
 
-# Copy only the requirements file to leverage Docker cache
-COPY . .
+# Copy the project files into the container
+COPY . /app
 
-# Install Python dependencies
+# Install dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Copy the entire project to the container
-COPY . .
-
-# Expose the Flask app port
-EXPOSE 5000
-
-# Set environment variables for Flask
-ENV FLASK_APP=app.py
-ENV FLASK_RUN_HOST=0.0.0.0
+# Copy the .dvc config file for authentication
+COPY .dvc/config.local /root/.dvc/config.local
 
 # Run the application
-CMD ["flask", "run", "--host=0.0.0.0"]
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -52,27 +52,4 @@ def predict():
         return render_template('index.html', prediction_text="Error occurred during prediction")
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-
-'''
-if __name__=="__main__":
-    logging.info("The execution has started")
-
-    try:
-        #data_ingestion_config=DataIngestionConfig()
-        data_ingestion=DataIngestion()
-        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
-
-        #data_transformation_config=DataTransformationConfig()
-        data_transformation=DataTransformation()
-        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
-
-        #model_trainer_config=ModelTrainerConfig()
-        model_trainer=ModelTrainer()
-        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
-        
-    except Exception as e:
-        logging.info("Custom Exception")
-        raise CustomException(e,sys)
-'''
+    app.run(debug=True, host='0.0.0.0', port=5000)
